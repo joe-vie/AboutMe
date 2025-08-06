f48c32b2d120614aa600afc8f87d2d6a3429fad2
Jovie <marcemg101@gmail.com>
2025-08-06 13:53:31 -0400

    first iteration of my AboutMe app

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemRed)
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .cornerRadius(15)
+                .padding(15)
+                .foregroundColor(.white)
+            VStack {
+                Text("Jovie")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("Here is a collection of images that truly reflect what has happened this past school year")
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Image("play1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300.0, height: 3`0.0)
+                    Image("play2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300.0, height: 300.0)
+                }
+                .frame(width: 200.0, height: 200.0)
+            }
         }
-        .padding()
     }
 }
 
